b6fa9771d15efa91ed9f3d254934f3225132deb2
hugo.alayrangues <hugo.alayrangues@orange.fr>
2017-11-07 11:46:34 +0100

    Modification de 'bloc_header.php' - V.1 - Ajout des principaux éléments de l'en-tête (les liens et image sont fictifs)

---
+++ site/bloc_page/bloc_header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Header</title>
+</head>
+
+<!--Header
+	Ce bloc apparaîtra sur la majorité des pages du site
+	Il comporte :
+		- un titre de page
+		- le logo d'Harvey
+		- un bouton pour accéder à la messagerie (contact Domisep)
+		- un bouton pour changer de langue (anglais/français)
+		- un bouton déconnexion
+--> 
+
+<body>
+	<h1>
+		Titre de la page
+	</h1>
+
+	<div id="logo">
+		<img src="#" alt="Logo d'Harvey"/>
+	</div>
+
+	<div id="messagerie">
+		<a href="#" title="Contacter Domisep"><img src="#" alt="Enveloppe"></a>		
+	</div>
+	
+	<div id="langue">
+		<a href="#" title="Traduire en français"><img src="#" alt="Drapeau français"></a>
+		<a href="#" title="Traduire en anglais"><img src="#" alt="Drapeau anglais"></a>
+	</div>
+
+	<div id="deconnexion">
+		<a href="#" title="Se déconnecter"><img src="#" alt="Bouton déconnexion"></a>
+	</div>
+
+</body>
+
+</html>
