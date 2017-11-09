bdf1da5d083fda6607169dfbe32407cbf87236e2
hugo.alayrangues <hugo.alayrangues@orange.fr>
2017-11-09 09:48:00 +0100

    Modification de 'bloc_nav.php' - V.1 - Ajout des liens du menu de navigation (les liens sont fictifs)

---
+++ site/bloc_page/bloc_nav.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Nav</title>
+</head>
+
+<!--Nav
+	Ce bloc apparaîtra sur la majorité des pages du site
+	Il comporte :
+		- un lien vers la page "Vue générale"
+		- un lien vers la page "Gestion des pièces"
+		- un lien vers la page "Gestion des capteurs"
+		- un lien vers la page "Profil utilisateur"
+		- un lien vers la page "Consommation énergétique"
+-->
+
+<body>
+	<nav>
+		<h1>Menu</h1>
+		<a href="#">Vue générale</a>
+		<a href="#">Gestion des pièces</a>
+		<a href="#">Gestion des capteurs</a>
+		<a href="#">Profil utilisateur</a>
+		<a href="#">Consommation énergétique</a>
+	</nav>
+
+</body>
+</html>
